48a39b31f817e845803619e2d7eedc150317f86e
Nikita Laptiev <nikita.laptiev.ukr@gmail.com>
2024-04-29 20:53:14 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const openModal = document.querySelector(".order-button");
 const closeModal = document.querySelector(".close-button");
 const backdrop = document.querySelector(".modal-overlay");
+const openMenuBtn = document.querySelector(".open-menu-btn");
+const mobMenu = document.querySelector(".mob-menu");
+const menuCloseBtn = document.querySelector(".menu-close-btn");
 
 openModal.onclick = () => {
   console.log(1);
@@ -11,4 +14,11 @@ openModal.onclick = () => {
 closeModal.onclick = () => {
   backdrop.classList.remove("open");
   backdrop.classList.add("close");
+};
+
+openMenuBtn.onclick = () => {
+  mobMenu.classList.add("is-open");
+};
+menuCloseBtn.onclick = () => {
+  mobMenu.classList.remove("is-open");
 };
